f23c90799e75f17bc1d724b4de535bd98bada81e
cupmen <cupmen@cupmen-macbook-air.local>
2023-01-13 16:43:37 +0900

    404ページの修正

--- pages/404.tsx
+++ pages/404.tsx
@@ -6,27 +6,15 @@ import { motion } from 'framer-motion';
 const NotFound: NextPage = () => {
   return (
     <>
-      <motion.div
-        initial={{ opacity: 0, y: 10 }} // 初期状態
-        animate={{ opacity: 1, y: 0 }} // マウント時
-        exit={{ opacity: 0, y: 10 }} // アンマウント時
-        transition={{
-          duration: 0.25,
-        }}
-        css={css`
-          width: 100vw;
-          height: 50vh;
-          position: relative;
-        `}
-      >
-        <Image src="/404.svg" alt="" layout="fill" />
-      </motion.div>
       <motion.span
         css={css`
           font-size: 10rem;
           font-family: futura-pt, sans-serif;
           font-weight: 700;
           font-style: italic;
+          text-align: center;
+          width: 100%;
+          display: inline-block;
         `}
         initial={{ opacity: 0, y: 10 }} // 初期状態
         animate={{ opacity: 1, y: 0 }} // マウント時
